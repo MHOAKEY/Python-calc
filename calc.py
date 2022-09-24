0bf59255b09e7b0737d11155aa439682f81b277c
Matt Oake <matt@192.168.2.16>
2022-09-24 20:31:10 -0230

    added comments and cleaned up spacing

--- calc.py
+++ calc.py
@@ -5,23 +5,37 @@
 
 exit = False
 while exit == False:
-    userNumber1=input("enter 1st number: ")
-    userOperator=input("+, -, * or /: ")
-    userNumber2=input("enter 2nd number: ")
-    
+    userNumber1 = input("enter 1st number: ")
+
+    # this if statement is designed to check if the user entered a digit instead of something else (i.e typo)
+    if userNumber1.isdigit() == False:
+        print("Can only use whole numbers")
+        continue
+
+    userOperator = input("+, -, * or /: ")
+    userNumber2 = input("enter 2nd number: ")
+
+    if userNumber2.isdigit() == False:
+        print("Can only use whole numbers")
+        continue
 
     if userOperator == "+":
-        userResult=int(userNumber1)+int(userNumber2)
+        userResult = int(userNumber1) + int(userNumber2)
         exit = True
+
     if userOperator == "-":
-        userResult=int(userNumber1)-int(userNumber2)
+        userResult = int(userNumber1) - int(userNumber2)
         exit = True
+
     if userOperator == "*":
-        userResult=int(userNumber1)*int(userNumber2)
+        userResult = int(userNumber1) * int(userNumber2)
         exit = True
+
     if userOperator == "/":
-        userResult=int(userNumber1)/int(userNumber2)
+        userResult = int(userNumber1) / int(userNumber2)
         exit = True
+
     else:
         userResult="I can only do + or -"
+        
     print(userResult)
