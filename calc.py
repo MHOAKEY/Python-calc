bb97986907bdd8a248142ae8bc430e3eef865917
Matt Oake <matt@192.168.2.16>
2022-09-24 19:45:41 -0230

    Basic calulator with if statements that loops until + or - is entered as an opperator

--- calc.py
+++ calc.py
@@ -1,1 +1,20 @@
-print("hello world")
+
+
+# for x in "banana":
+#   print(x)
+
+exit = False
+while exit == False:
+    userNumber1=input("enter 1st number")
+    userNumber2=input("enter 2nd number")
+    userOperator=input("+ or -")
+
+    if userOperator == "+":
+        userResult=int(userNumber1)+int(userNumber2)
+        exit = True
+    if userOperator == "-":
+        userResult=int(userNumber1)-int(userNumber2)
+        exit = True
+    else:
+        userResult="I can only do + or -"
+    print(userResult)
