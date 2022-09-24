ac0e0044603ebd84665a5f7a1d7750febbf38a44
Matt Oake <matt@192.168.2.16>
2022-09-24 19:53:09 -0230

    Added multiplication to variables

--- calc.py
+++ calc.py
@@ -5,15 +5,19 @@
 
 exit = False
 while exit == False:
-    userNumber1=input("enter 1st number")
-    userNumber2=input("enter 2nd number")
-    userOperator=input("+ or -")
+    userNumber1=input("enter 1st number: ")
+    userOperator=input("+, - or *: ")
+    userNumber2=input("enter 2nd number: ")
+    
 
     if userOperator == "+":
         userResult=int(userNumber1)+int(userNumber2)
         exit = True
     if userOperator == "-":
         userResult=int(userNumber1)-int(userNumber2)
+        exit = True
+    if userOperator == "*":
+        userResult=int(userNumber1)*int(userNumber2)
         exit = True
     else:
         userResult="I can only do + or -"
